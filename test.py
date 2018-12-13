500e05760f1f7dac937423a211c8f41fc1aa1354
Eli Lipsitz <eli.lipsitz@gmail.com>
2018-12-13 03:42:16 -0800

    switch to nacl library

--- test.py
+++ test.py
@@ -1,81 +1,39 @@
-from Cryptodome.PublicKey import RSA, ECC
-from Cryptodome.Hash import SHA256
-from Cryptodome.Random import get_random_bytes
-from Cryptodome.Cipher import AES, PKCS1_OAEP
-from Cryptodome.Signature import DSS
+from nacl.public import PrivateKey, SealedBox
+import nacl.encoding
+from nacl.signing import SigningKey
+import nacl.utils
+import nacl.hash
 
 from contexttimer import Timer
 
-import json
+import pickle
 from base64 import b64encode, b64decode
 
 
-NUM_USERS = 2
-RSA_BITS = 2048
+NUM_USERS = 10
 DB_BITS = 256
 
 #### utils:
-def generate_ecc_key():
-    """
-    Returns (public key, secret key)
-    """
-    ecc_key = ECC.generate(curve='P-256')
-    return ecc_key.public_key(), ecc_key
-
-def generate_rsa_key():
-    rsa_key = RSA.generate(2048)
-    return rsa_key.publickey(), rsa_key
-
 def makehash(data):
-    print(data)
+    #print(data)
+    data = obj2bytes(data)
+    return nacl.hash.sha256(data, encoder=nacl.encoding.HexEncoder)
+
+def obj2bytes(data):
     if type(data) is not bytes:
-        data = json.dumps(data).encode('utf-8')
-    return SHA256.new(data)
+        data = pickle.dumps(data)
+    return data
 
-def sign(key, data):
-    signer = DSS.new(key, 'fips-186-3')
-    signature = signer.sign(makehash(data))
-    signature = b64encode(signature).decode('utf-8')
-    return signature, data
-
-def verify_signature(key, data):
-    signature, contents = data
-    signature = b64decode(signature)
-    verifier = DSS.new(key, 'fips-186-3')
-    verifier.verify(makehash(data), signature)
+def bytes2obj(data):
+    return pickle.loads(data)
 
 def get_key_string(key):
-    return key.export_key(format='PEM')
-
-def public_encrypt(key, data):
-    if type(data) is not bytes:
-        data = json.dumps(data).encode('utf-8')
-
-    session_key = get_random_bytes(16)
-
-    cipher_rsa = PKCS1_OAEP.new(key)
-    enc_session_key = cipher_rsa.encrypt(session_key)
-
-    cipher_aes = AES.new(session_key, AES.MODE_GCM)
-    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-    output = (enc_session_key, cipher_aes.nonce, tag, ciphertext)
-    return output
-
-def public_decrypt(key, data, decode_json=True):
-    enc_session_key, nonce, tag, ciphertext = data
-
-    cipher_rsa = PKCS1_OAEP.new(private_key)
-    session_key = cipher_rsa.decrypt(enc_session_key)
-
-    cipher_aes = AES.new(session_key, AES.MODE_GCM, nonce)
-    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-    if decode_json:
-        return json.loads(data.decode("utf-8"))
-    return data
+    return key.encode(encoder=nacl.encoding.HexEncoder)
 
 class KeyAuthority:
     def __init__(self):
-        self.public_key, self.secret_key = generate_ecc_key()
+        self.signing_key = SigningKey.generate()
+        self.verify_key = self.signing_key.verify_key
         # self.pk_to_db = {}
         self.db_to_pk = {}
 
@@ -97,37 +55,39 @@ class KeyAuthority:
         if user_public_key_str not in voting_group:
             raise Exception('KeyAuthority: user PK not in voting group')
 
-        voting_group_hash = makehash(voting_group).hexdigest()
+        voting_group_hash = makehash(voting_group)
         
-        user_token = makehash((epoch, voting_group, user_public_key_str, secret_key)).hexdigest()
-        pdp = sign(self.secret_key, (distance_result, voting_group_hash, user_token))
-        sdp = sign(self.secret_key, (user_public_key_str, pdp))
-        esdp = public_encrypt(user_public_key, sdp)
+        user_token = makehash((epoch, voting_group, user_public_key_str, secret_key))
+        pdp = self.signing_key.sign(obj2bytes((distance_result, voting_group_hash, user_token)))
+        sdp = self.signing_key.sign(obj2bytes((user_public_key_str, pdp)))
+        esdp = SealedBox(user_public_key).encrypt(sdp)
         return esdp
 
 class DistanceVerifier:
     def __init__(self):
-        self.public_key, self.secret_key = generate_ecc_key()
+        self.signing_key = SigningKey.generate()
+        self.verify_key = self.signing_key.verify_key
 
     def set_voting_group(self, group):
         self.voting_group = group
 
     def request_distance_verification(self, device, epoch):
         # ok! let's do distance verification
-        challenge = get_random_bytes(DB_BITS // 8)
+        challenge = nacl.utils.random(DB_BITS // 8)
 
         distance = device.distance
         response = device.handle_db_challenge(challenge)
 
-        distance_result = sign(self.secret_key, (distance, epoch))
+        distance_result = self.signing_key.sign(obj2bytes((distance, epoch)))
         esdp = key_authority.complete_distance_verification(challenge, response, epoch, self.voting_group, distance_result)
         return esdp
 
 
 class User:
     def __init__(self, distance):
-        self.public_key, self.secret_key = generate_rsa_key()
-        self.db_key = b64encode(get_random_bytes(DB_BITS // 8)).decode('utf-8')
+        self.private_key = PrivateKey.generate()
+        self.public_key = self.private_key.public_key
+        self.db_key = nacl.utils.random(DB_BITS // 8)
         self.public_key_str = get_key_string(self.public_key)
         self.distance = distance
 
